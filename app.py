aaf415f3a6c207a6e223f6f4184732f0bf963ff5
Valeriia223 <132986539+Valeriia223@users.noreply.github.com>
2023-09-20 20:51:59 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+import telebot
+from config import keys, TOKEN
+from extensions import APIException, CryptoConverter
+
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def bot_test(message: telebot.types.Message):
+    text = 'Чтобы начать работу, введите команду боту в следующем формате:\n<имя валюты> \
+<в какую валюту перевести>\
+<количество переводимой валюты>\nУвидеть список доступных валют: /values'
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = "Доступные валюты: "
+    for key in keys.keys():
+        text = '\n'.join((text, key, ))
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(content_types=['text', ])
+def get_price(message: telebot.types.Message):
+    try:
+        values = message.text.split(' ')
+
+        if len(values) != 3:
+            raise APIException('Неверное количество параметров.')
+
+        base, quote, amount = values
+        total_quote = CryptoConverter.get_price(base, quote, amount)
+    except APIException as e:
+        bot.reply_to(message, f'Ошибка пользователя.\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду\n{e}')
+    else:
+        text = f'Цена {amount} {base} в {quote} - {total_quote * float(amount)}'
+        bot.send_message(message.chat.id, text)
+
+
+bot.polling()
---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN = '6644604415:AAFkkn-lWdLE4u4ZEVU8NlVNasMSQ7aFqBc'
+
+keys = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубли': 'RUB'
+}
